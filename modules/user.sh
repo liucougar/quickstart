d7977ea708990d9c0cda317717fac58dca42b102
liucougar <liucougar@gmail.com>
2011-10-28 16:57:34 -0700

    make sure randomly generated passwords does not have ' or "
    make add_ssh_config work properly
    
    fixed a typo in add_ssh_file

--- modules/user.sh
+++ modules/user.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 random_password() {
     local len=${1:-"12"}
-    #echo $(</dev/urandom tr -dc A-Za-z0-9\*\&\\-\!\@\#\$\%\(\)| (head -c $1 > /dev/null 2>&1 || head -c ${len}))
-    echo "$(tr -dc '[:print:]' < /dev/urandom | fold -w ${len} |grep .*[0-9].*[0-9].* |grep [^0-9]\$ | grep .*[A-Z].*[A-Z].* | grep ^[a-zA-Z] | head -n 1)"
+    #disallow '" in the password
+    echo "$(tr -dc '[:print:]' < /dev/urandom | fold -w ${len} |grep .*[0-9].*[0-9].* |grep [^0-9]\$ | grep .*[A-Z].*[A-Z].* | grep ^[a-zA-Z] | grep -v [\'\"] | head -n 1)"
 }
 
 add_user() {
@@ -27,12 +27,12 @@ add_ssh_file() {
     local abshomedir="${chroot_dir}$homedir"
     if [ -n "$abshomedir" -a -d "$abshomedir" ]; then
 	#create the .ssh dir as the user if not already exist
-        spawn_chroot "sudo -u $user mkdir -p \"${abshomedir}/.ssh/\""
-        cp "$idfile" "${abshomedir}/.ssh/${filename}"
+        spawn_chroot "su -c 'mkdir -p \"${homedir}/.ssh/\"' $user" || die "failed to mkdir $abshomedir/.ssh dir"
+        cp "$idfile" "${abshomedir}/.ssh/${filename}" || die "failed to copy $idfile to ${abshomedir}/.ssh/${filename}"
 	#spawn_chroot is needed because the user may not exist on host
-	spawn_chroot "chown ${user}:${user} $homedir/.ssh/${filename}"
+	spawn_chroot "chown ${user}:${user} $homedir/.ssh/${filename}" || die "failed to chown $homedir/.ssh/${filename}"
         if [ "${filename:0:3}" = "id_" ]; then #id_rsa or similar, set proper permission
-            chmod 600 $abshomedir/.ssh/${filename}
+            chmod 600 $abshomedir/.ssh/${filename} || die "failed to chmod"
         fi
     else
         die "add_ssh_file() Unknown user $user"
@@ -40,18 +40,12 @@ add_ssh_file() {
 }
 
 add_ssh_config() {
-    local user=$1
-    local filename=$2
+    local user="$1"
+    local filename="$2"
     local value="$3"
 
-    local homedir="$(find_user_home "$user")"
-    local abshomedir="${chroot_dir}$homedir"
-    if [ -n "$abshomedir" -a -d "$abshomedir" ]; then
-        #create the .ssh dir as the user if not already exist
-	spawn_chroot "sudo -u $user bash -c \"mkdir -p '${homedir}/.ssh/' && echo '$value' >> ${homedir}/.ssh/${filename}\""
-        #echo "$value" >> "$abshomedir/.ssh/$filename"
-	#spawn_chroot "chown ${user}:${user} $homedir/.ssh/${filename}"
-    else
-        die "add_ssh_config($filename) Unknown user $user"
-    fi
+    local tmpfile="$(tempfile)"
+    echo "$value" > "$tmpfile"
+    add_ssh_file "$user" "$tmpfile" "$filename"
+    rm -f "$tempfile"
 }
